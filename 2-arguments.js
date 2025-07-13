4e0f41eea9a3e4a2cdb435a3211878636c9be8a0
Malika Edaif <edmalika957@gmail.com>
2025-07-13 18:38:59 +0100

    create file 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,16 +1,15 @@
-function arg(name){
+const args = process.argv.slice(2);
+const count = args.length;
 
-    if (name == undefined){
-    console.log("No argument");}
-    else if (arguments.length==1){
-        console.log("Argument found");
-    }
-    else {
-    console.log("Argument found");}
-   }
 
-   arg();
-   arg("best");
-   arg("best school");
+if (count === 0) {
+    console.log("No argument");
+} else if (count === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
+
+
 
 
